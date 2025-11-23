7380be4485ab4f649b9e4aa9e7b4589dddd99d69
Leo Constantin <lcon69184@gmail.com>
2025-11-23 16:22:06 +0200

    first commit

---
+++ components/ConvexClientProvider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ReactNode } from "react";
+import { ConvexReactClient } from "convex/react";
+
+import { authClient } from "@/lib/auth-client";
+import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!, {
+  // Optionally pause queries until the user is authenticated
+  expectAuth: true,
+});
+
+export default function ConvexClientProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <ConvexBetterAuthProvider client={convex} authClient={authClient}>
+      {children}
+    </ConvexBetterAuthProvider>
+  );
+}
